44335bc6fac6fc2cd9858e6077d849d829602b76
Henning Pingel <post@henningpingel.de>
2020-08-04 22:36:14 +0200

    adding missing menuItemStr param to calls of handleC64

--- c64screen.cpp
+++ c64screen.cpp
@@ -524,7 +524,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		if ( k == 139 /* F6 */ )
 		{
 			menuScreen = MENU_NETWORK;
-			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal );
+			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 			return;
 		}
 #endif
@@ -1089,7 +1089,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		if ( k == 136 /* F7 */ || k == 139 /* F6 */)
 		{
 			menuScreen = MENU_MAIN;
-			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal );
+			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 			return;
 		}
 		if ( k == 'x' || k == 'X')
@@ -1098,7 +1098,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 			{
 				pSidekickNet->redrawSktxScreen();
 				menuScreen = MENU_SKTX;
-				handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal );
+				handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 				return;
 			}
 		}
@@ -1129,7 +1129,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		if ( k == 136 /* F7 */ || k == 139 /* F6 */)
 		{
 			menuScreen = MENU_MAIN;
-			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal );
+			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 			return;
 		}
 		/*
@@ -1174,7 +1174,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		{
 			//jump to network menu from config menu - do we really need this?
 			menuScreen = MENU_NETWORK;
-			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal );
+			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 			return;
 		}		
 #endif
