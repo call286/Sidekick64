553f8b1a42067dd19efa90a133096c2f1c69e3b8
Henning Pingel <post@henningpingel.de>
2020-10-06 18:48:41 +0200

    case 10 was taken by upstream, use 11...

--- kernel_menu.cpp
+++ kernel_menu.cpp
@@ -965,7 +965,7 @@ int main( void )
 		case 5:
 			KernelEFRun( kernel.m_InputPin, &kernel, FILENAME, menuItemStr, false, NULL, 0 );
 			break;
-		case 10:
+		case 11:
 			KernelEFRun( kernel.m_InputPin, &kernel, FILENAME, menuItemStr, true, prgDataLaunch, prgSizeLaunch );
 			break;
 		case 6:
