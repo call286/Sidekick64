50a83432324e6197ce1515c9eaae6d88ea8fd044
Henning Pingel <post@henningpingel.de>
2020-11-06 00:10:51 +0100

    deactivate outdated functionality

--- c64screen.cpp
+++ c64screen.cpp
@@ -1212,7 +1212,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		{
 			pSidekickNet->requestReboot();
 		}
-		
+		/*
 		else if ( k == 'u' || k == 'U')
 		{
 			if (pSidekickNet->IsRunning())
@@ -1220,7 +1220,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 				pSidekickNet->queueKernelUpdate();
 				setErrorMsg( pSidekickNet->getNetworkActionStatusMessage() );
 			}
-		}
+		}*/
 		else if ( k == 'd' || k == 'D')
 		{
 				if ( errorMsg != NULL ) errorMsg = NULL;
