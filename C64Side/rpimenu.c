40c062e827b0460e8ccd83211921a78cc92f4fca
hpingel <post@henningpingel.de>
2021-01-13 20:51:39 +0100

    avoid automated keypresses, indention: replace tabs by spaces

--- C64Side/rpimenu.c
+++ C64Side/rpimenu.c
@@ -212,7 +212,7 @@ void readSIDKick_Version()
 int main (void)
 {
     char key, x, firstHit;
-	unsigned char /*joy1, joy1prev, */joy2, joy2prev;
+  unsigned char /*joy1, joy1prev, */joy2, joy2prev;
 
     *(unsigned char*)(0x01) = 15;
 
@@ -236,7 +236,7 @@ int main (void)
     copyCharset();
     updateScreen();
 
-	joy_install( joy_static_stddrv );
+  joy_install( joy_static_stddrv );
 
     wireDetection();
     *((char *)(0xdf01)) = 0; // dummy keypress
@@ -246,9 +246,9 @@ int main (void)
     *((char *)(0xdf01)) = 0; // dummy keypress
     updateScreen();
 
-		instNMIHandler();
+    instNMIHandler();
 
-	joy2prev = 255;
+    joy2prev = 255;
     while ( 1 )
     {
 		key = 0;
@@ -332,6 +332,10 @@ int main (void)
 			for ( x = 0; x < 15; x++ )
 				waitvsync();
     }
+    if ( firstHit )
+      for ( x = 0; x < 15; x++ )
+        waitvsync();
+  }
 
-    return 0;
+  return 0;
 }
