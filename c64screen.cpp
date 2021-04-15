a8278109e15a3ac8bf38465bfecbf929c800b0c8
hpingel <post@henningpingel.de>
2021-04-15 23:20:34 +0200

    use toggle key m for modem

--- c64screen.cpp
+++ c64screen.cpp
@@ -1459,7 +1459,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 				setErrorMsg( pSidekickNet->getNetworkActionStatusMessage() );
 			}
 		}
-		else if ( k == 't' || k == 'T')
+		else if ( k == 'm' || k == 'M')
 		{
 			if (pSidekickNet->IsRunning())
 			{
@@ -1878,7 +1878,7 @@ void printNetworkScreen()
 	CString strHostName   = "Hostname:           ";
 	CString strConnection = "Connection state:   ";
 	CString strWebserver  = "Webserver state:    ";
-	CString strUPModemEmu = "Modem emulation (T):";
+	CString strUPModemEmu = "Modem emulation (M):";
 	CString strHelper;
 	
 	if (strcmp(netSidekickHostname,"") != 0)
