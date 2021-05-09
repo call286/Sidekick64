bc133b4841ffbaca6757d7fdabaedcb1067f3f31
hpingel <post@henningpingel.de>
2021-05-09 12:59:10 +0200

    missing line break after rebase fixed

--- kernel_sid.cpp
+++ kernel_sid.cpp
@@ -1693,7 +1693,8 @@ void CKernel::FIQHandler (void *pParam)
 			#else
 			write32( ARM_PWM_DAT1, d1 );
 			write32( ARM_PWM_DAT2, d2 );
-			#endif			RESET_CPU_CYCLE_COUNTER
+			#endif
+			RESET_CPU_CYCLE_COUNTER
 			return;
 		} 
 	}
